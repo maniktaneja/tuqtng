1a5ce050d35886cb8c970fde9700488ece992c3f
Sarath Lakshman <sarathlakshman@slynux.com>
2014-09-20 23:02:29 +0530

    add logging for query evaluater

--- hack/main.go
+++ hack/main.go
@@ -1,14 +1,15 @@
-package main 
+package main
+
 import (
-    "github.com/nimishzynga/goweb"
-    "fmt"
-    "net/http"
+	"fmt"
+	"github.com/nimishzynga/goweb"
+	"net/http"
 )
 
 func setupHandler() {
-goweb.MapFunc("/queryDoc", func(c *goweb.Context) {
-        HandleDoc(c)
-    })
+	goweb.MapFunc("/queryDoc", func(c *goweb.Context) {
+		HandleDoc(c)
+	})
 }
 
 func runExpr(doc []byte, expr []string) string {
@@ -19,31 +20,32 @@ func runExpr(doc []byte, expr []string) string {
 	secKey, err := N1QLTransform(doc, cExpr)
 	if err != nil {
 	}
-    return string(secKey)
+	return string(secKey)
 }
 
 type Doc struct {
-    //DocData map[string]string
-    DocData string
-    DocExpr []string
+	//DocData map[string]string
+	DocData string
+	DocExpr []string
 }
 
 func HandleDoc(c *goweb.Context) {
-    fmt.Println("dfdfd")
-    if c.IsPost() || c.IsPut() {
-        //NewDoc := Doc{DocData: make(map[string]string)}
-        NewDoc := Doc{}
-        if err := c.Fill(&NewDoc); err != nil {
-            return
-        }
-        fmt.Println("query data is", NewDoc.DocData)
-        data := runExpr([]byte(NewDoc.DocData), NewDoc.DocExpr)
-        c.WriteResponse(data, 200)
-    }
+	fmt.Println("dfdfd")
+	if c.IsPost() || c.IsPut() {
+		//NewDoc := Doc{DocData: make(map[string]string)}
+		NewDoc := Doc{}
+		if err := c.Fill(&NewDoc); err != nil {
+			return
+		}
+		fmt.Println("query doc = ", NewDoc.DocData, "expr = ", NewDoc.DocExpr)
+		data := runExpr([]byte(NewDoc.DocData), NewDoc.DocExpr)
+		fmt.Println("map result = ", data)
+		c.WriteResponse(data, 200)
+	}
 }
 
 func main() {
-    goweb.ConfigureDefaultFormatters()
-    setupHandler()
-    http.ListenAndServe(":10000", goweb.DefaultHttpHandler)
+	goweb.ConfigureDefaultFormatters()
+	setupHandler()
+	http.ListenAndServe(":10000", goweb.DefaultHttpHandler)
 }
